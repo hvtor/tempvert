f7d38eb38e3f3fa6cce9c23cb995e9cac5c0435b
Hemant Torsekar <hvt@me.com>
2015-04-17 15:10:21 -0400

    Initial Commit

---
+++ TempVert/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TempVert
+//
+//  Created by Hemant V. Torsekar on 2015-04-17.
+//  Copyright (c) 2015 Hemant V. Torsekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
